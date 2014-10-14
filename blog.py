ab7cbf80e866cc0d6da73dcd8fe139ac500eb228
mikamikuh <mkouhei0910@gmail.com>
2014-10-14 00:58:36 -0400

    Add function to show posts.

--- blog.py
+++ blog.py
@@ -2,6 +2,7 @@
 
 # imports
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+from functools import wraps
 import sqlite3
 
 # configuration
@@ -18,6 +19,16 @@ app.config.from_object(__name__)
 # function used for connecting to the database
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -37,6 +48,7 @@ def logout():
 	return redirect(url_for('login'))
 
 @app.route('/main')
+@login_required
 def main():
 	return render_template('main.html')
 
